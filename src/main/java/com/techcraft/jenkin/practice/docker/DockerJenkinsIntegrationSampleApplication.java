a837223785f791e86bccb6ede211b1ef94a1055b
Java Techcraft <java528.techcraft@gmail.com>
2022-08-05 18:57:09 +0100

    added controller class

--- src/main/java/com/techcraft/jenkin/practice/docker/DockerJenkinsIntegrationSampleApplication.java
+++ src/main/java/com/techcraft/jenkin/practice/docker/DockerJenkinsIntegrationSampleApplication.java
@@ -1,13 +1,19 @@
 package com.techcraft.jenkin.practice.docker;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DockerJenkinsIntegrationSampleApplication {
 
+	public static Logger logger = LoggerFactory.getLogger(DockerJenkinsIntegrationSampleApplication.class);
+
 	public static void main(String[] args) {
+		logger.info("Entered into class DockerJenkinsIntegrationSampleApplication....");
 		SpringApplication.run(DockerJenkinsIntegrationSampleApplication.class, args);
+		logger.info("Ending ***** class DockerJenkinsIntegrationSampleApplication....");
 	}
 
 }
